78cdfc70ac61a62871e87d9574a7bbcc975dc5d7
danshook <shook.dan@gmail.com>
2018-06-21 19:19:58 -0700

    added click cards

--- src/ClickCard/ClickCard.js
+++ src/ClickCard/ClickCard.js
@@ -4,6 +4,25 @@ import Image from "../Image/Image";
 
 
 class ClickCard extends React.Component {
+
+  state = {
+    numOfTimesClicked: 0
+  }
+
+  handleClick = () => {
+
+    
+
+    // this.state.numOfTimesClick++;
+    this.setState({
+      numOfTimesClicked: this.state.numOfTimesClicked + 1
+    }, () => {
+      if(this.state.numOfTimesClicked > 1) {
+        window.alert('Game over!')
+      }
+    })
+  }
+
   render() {
     return (
       <div>
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -6,9 +6,16 @@ import ClickCard from "../ClickCard/ClickCard";
 
 class Main extends React.Component {
   render() {
+    console.log("Main renders!");
     return (
       <div>
-          <p>lorem lsadkfjs;dfjsl;dfkjsdfl</p>
+          {/* <p>lorem lsadkfjs;dfjsl;dfkjsdfl</p> */}
+          <ClickCard myCoolImage="https://static1.srcdn.com/wordpress/wp-content/uploads/2018/04/Rick-and-Morty-Run-the-Jewels.jpg" />
+          <ClickCard />
+          <ClickCard />
+          <ClickCard />
+          <ClickCard />
+          <ClickCard />
           <ClickCard />
       </div>
     );
